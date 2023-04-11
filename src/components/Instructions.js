79397685e14a57331316c19fc9772b6c0b84dc03
Paulius Kurlavičius <paul.kurlavicius@gmail.com>
2023-04-11 12:06:42 +0300

    Update instructions

--- src/components/Instructions.js
+++ src/components/Instructions.js
@@ -4,25 +4,25 @@ import Modal from "./Modal/Modal";
 const Instructions = (props) => {
     return (
         <Modal onClose={props.onClose}>
-            <h3>Instructions for the workout</h3>
+            <h3>Workout Instructions</h3>
             <ol>
-                <li>For pairs</li>
-                    <ul>
-                        <li>do set of the first exercise</li>
-                        <li>rest for 90 seconds</li>
-                        <li>dot set of the second exercise</li>
-                        <li>rest for 90 seconds</li>
-                        <li>repeat</li>
-                    </ul>
-                <li>For the triplet</li>
-                    <ul>
-                        <li>everything is the same except rest is 60 seconds and there are 3 exercises</li>
-                    </ul>
+                <li>For exercise pairs:</li>
+                <ul>
+                    <li>Complete a set of the first exercise.</li>
+                    <li>Take a 90-second break.</li>
+                    <li>Perform a set of the second exercise.</li>
+                    <li>Rest for another 90 seconds.</li>
+                    <li>Repeat this process.</li>
+                </ul>
+                <li>For the exercise triplet:</li>
+                <ul>
+                    <li>Follow the same procedure as for pairs, but reduce the rest time to 60 seconds and include a third exercise.</li>
+                </ul>
             </ol>
-            <h3>Instructions for the app</h3>
+            <h3>App Usage Instructions</h3>
             <ol>
-                <li>To cycle progressions tap on either side of the video</li>
-                <li>"Do 3x5-8" means do 3 sets of 5-8 reps</li>
+                <li>To switch between exercise progressions, tap either side of the video display.</li>
+                <li>When you see "Do 3x5-8," it means to complete 3 sets with 5 to 8 repetitions per set.</li>
             </ol>
             <div className="instrLayout">
                 <a href="https://www.reddit.com/r/bodyweightfitness/wiki/kb/recommended_routine/#wiki_welcome_to_the_recommended_routine">More info</a>
